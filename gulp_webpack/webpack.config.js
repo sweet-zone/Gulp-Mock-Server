2beac9b75502ae6069c50ef7445bfba1798a6abd
zjzhome <zjzhome@126.com>
2015-11-30 09:52:13 +0800

    add gulp with webpack

---
+++ gulp_webpack/webpack.config.js
@@ -0,0 +1,27 @@
+
+var path = require('path');
+var webpack = require('webpack');
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+
+module.exports = {
+    entry: {
+        index: './src/javascript/index.js',
+        login: './src/javascript/login.js'
+    }, 
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+            test: /\.mcss$/,
+            loader: 'style-loader!css-loader?sourceMap!mcss-loader?sourceMap'
+        }, {
+            test: /\.rgl$/,
+            loader: 'rgl-loader'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('common.js', ['index', 'login'])
+    ]
+}
