33d42ab950afd5abb4e289d24a45882e54f6307f
zjzhome <zjzhome@126.com>
2016-04-21 17:29:23 +0800

    change server

--- gulpfile.js
+++ gulpfile.js
@@ -59,27 +59,6 @@ gulp.task('livereload:connect', () => {
 
 
 // -------- express服务器 ----------------
-function startExpress() {
-    let bodyParser = require('body-parser');
-    app.use(require('connect-livereload')());
-    app.use(express.static(EXPRESS_ROOT));
-    app.use(bodyParser.json()); // for parsing application/json
-    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-   let apis = require(APICONFIG);
-   for(let key in apis) {
-       let method = key.split(/\s+/)[0].toLowerCase(),
-           url = key.split(/\s+/)[1];
-
-       app[method](url, apis[key]);
-   }
-
-    let server = app.listen(EXPRESS_PORT, function() {
-        let host = server.address().address;
-        let port = server.address().port;
-        console.log('async server listening at http://%s:%s', host, port)
-    });
-}
 function startLivereload() {
     lr.listen(LIVEPORT, function() {
         console.log('livereload listen at port ' + LIVEPORT);
